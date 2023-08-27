a062d3ecc4914dc24b6eb41b9088eaa1edae7687
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2023-08-27 17:03:06 -0300

    Going crazy with tasks won't increase thread count
    This started with 15 threads.
    It turned into 19 with the Callback API.
    By adding PPL and coroutines we went up to 21.
    Move from 1 to 10000 gRPC calls remain the same thread count.

--- app.cpp
+++ app.cpp
@@ -7,6 +7,17 @@ int main(int argc, char* argv[]) {
   Client client(Endpoint{"127.0.0.1", Config::port});
   std::cout << "Running on main thread ID " << std::this_thread::get_id() << '\n';
   std::string user("I Am");
+
+  static constexpr size_t                     calls = 10000;
+  std::vector<concurrency::task<std::string>> queue;
+  queue.reserve(calls);
+  for (int i = 0; i < calls; i++) {
+    queue.emplace_back(client.SayHello(std::format("{} {}", user, i)));
+  }
+
+  auto joint = concurrency::when_all(std ::begin(queue), std::end(queue));
+  joint.wait();
+
   std::string reply = client.SayHello(user).get();
   std::cout << "Received: " << reply << std::endl;
 
